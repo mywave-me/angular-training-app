38c41d51b37119ca554a55e6e5bd766dbe4f758c
Leah Garrett <leah.garrett@mywave.me>
2020-03-10 11:31:36 +1100

    removed comment from config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 const {MWSdk, MWSdkConfig, Date: MyDate} = mywaveAiJsSdk
-// const mwSdkConfig = new MWSdkConfig('https://mywave-ai-training-be.app.mywave.me', 'https://mywave-ai-training-pps.app.mywave.me')
-const mwSdkConfig = new MWSdkConfig('http://localhost:8080', 'http://localhost:8080')
+const mwSdkConfig = new MWSdkConfig('https://mywave-ai-training-be.app.mywave.me', 'https://mywave-ai-training-pps.app.mywave.me')
  
 mwSdkConfig.withDetectEntityUpdatesInterval(30000)
   .withRequestTimeOut(10000)
